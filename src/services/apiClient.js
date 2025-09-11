d411563615080a0234d4cd1c9b5807d513c52ec2
Solomon Bhaskar <131343533+solomon712@users.noreply.github.com>
2025-09-11 13:01:29 +0530

    Updated new base url

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL:
-    import.meta.env.VITE_API || 'https://app-azuresearch-qa-emo.azurewebsites.net', //API HAS BEEN CHANGED
+    import.meta.env.VITE_API || 'http://app-azuresearch-qa-evolve.azurewebsites.net', //API HAS BEEN CHANGED http://app-azuresearch-qa-evolve.azurewebsites.net  //https://app-azuresearch-qa-emo.azurewebsites.net
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
